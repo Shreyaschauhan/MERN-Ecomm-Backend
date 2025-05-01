2d2b32cacb87820bc8575376a7a8659efecedac8
Shreyaschauhan <156332686+Shreyaschauhan@users.noreply.github.com>
2025-05-01 15:50:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,18 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'DELETE', 'PUT'],
-        allowedHeaders: [
-            "content-Type",
-            'Authorizzation',
-            'Cache-Control',
-            'Expires',
-            'pragma'
-        ],
-        credentials : true,
-    })
+    cors()
 );
 
 app.use(cookieParser());
